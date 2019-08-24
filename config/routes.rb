53ae8f990f001c1406623a56fb896390098bcd6f
Lisbeth Wu <meow@liswu.me>
2019-08-24 21:52:04 +0800

    routes: redirect when subdomain is `duan`

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :short_urls
+  get ':short_url' => 'short_urls#redirection', constraints: { subdomain: 'duan' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#under_maintainence'
   get '*path' => redirect(path: '/', status: 302)
